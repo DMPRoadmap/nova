d286e1d169827fef5952b829724f14723fbe3dd0
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-06-18 23:43:22 +0100

    extending contributors-controller

---
+++ app/controllers/nova/contributors_controller.rb
@@ -0,0 +1,12 @@
+module Nova 
+
+  class ContributorsController < ::ContributorsController
+    before_action :prefer_engine_views
+
+    private 
+
+    def prefer_engine_views
+      prepend_view_path Nova::Engine.root.join("app/views")
+    end
+  end
+end
