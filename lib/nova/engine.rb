e388d22b0740dd9fe0459c7dde7b7042a50374d8
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-06-19 00:43:01 +0100

    updated nova engine to prepend engine views to host app's views

--- lib/nova/engine.rb
+++ lib/nova/engine.rb
@@ -1,5 +1,11 @@
 module Nova
   class Engine < ::Rails::Engine
     isolate_namespace Nova
+
+    initializer "nova.prepend_view_paths" do 
+      ActiveSupport.on_load(:action_controller) do 
+        prepend_view_path Nova::Engine.root.join("app/views")
+      end
+    end
   end
 end
