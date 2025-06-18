17e17300777b4c450973a6b5133e59a015e39858
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-06-18 22:18:16 +0100

    moved the contributors form

--- app/controllers/nova/application_controller.rb
+++ app/controllers/nova/application_controller.rb
@@ -1,4 +1,11 @@
 module Nova
   class ApplicationController < ActionController::Base
+    before_action :prefer_engine_views
+
+    private 
+
+    def prefer_engine_views
+      prepend_view_path Nova::Engine.root.join("app/views")
+    end
   end
 end
