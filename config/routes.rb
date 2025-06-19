2a502085bef54e26b08ee44a86f344708080ec4f
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-06-19 11:26:35 +0100

    wrapped contributors routes in plans routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Nova::Engine.routes.draw do
-  resources: contributors, except: %i[show], to: "nova/contributors"
+  resources :plans do 
+    resources: contributors, except: %i[show], to: "nova/contributors"
+  end
   
   get "orcid_records(/:orcid_id)", to: "nova/orcid_records#show"
 end
