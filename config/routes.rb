343e090e3b29337fd54dfd9e765a48654af287e1
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-06-19 14:59:04 +0100

    fixed typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Nova::Engine.routes.draw do
   resources :plans do 
-    resources: contributors, except: %i[show], to: "nova/contributors"
+    resources :contributors, except: %i[show], to: "nova/contributors"
   end
   
   get "orcid_records(/:orcid_id)", to: "nova/orcid_records#show"
