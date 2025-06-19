cd2ccd90cf0a7bb3ce56b6eabd5fb193cd85556d
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-06-19 11:12:47 +0100

    tweaking

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Nova::Engine.routes.draw do
-  resources: contributors, to: "nova/contributors"
+  resources: contributors, except: %i[show], to: "nova/contributors"
   
   get "orcid_records(/:orcid_id)", to: "nova/orcid_records#show"
 end
