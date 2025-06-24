f0561a50acc1f8c2dc6a9b09dc5b2b3cece5ab8e
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-06-24 16:19:34 +0100

    removed reference to engine's assets

--- lib/nova/engine.rb
+++ lib/nova/engine.rb
@@ -8,10 +8,5 @@ module Nova
       end
     end
 
-    # Expose assets to host app
-    initializer "nova.assets.precompile" do |app|
-      app.config.assets.precompile += %w[nova/application.js]
-    end
-
   end
 end
