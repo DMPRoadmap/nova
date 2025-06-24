d6bc9db61026b978ddda24b1a8b24867e1c9969b
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-06-24 12:37:42 +0100

    removed redundant files

--- app/controllers/nova/application_controller.rb
+++ app/controllers/nova/application_controller.rb
@@ -1,6 +1,7 @@
 module Nova
   class ApplicationController < ActionController::Base
     include Pundit::Authorization
-    layout "nova_host_wrapped"
+    # layout "nova_host_wrapped"
+    layout "application"
   end
 end
